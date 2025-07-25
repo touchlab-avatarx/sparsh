1e8c97a1ba2c668d4d382b0422f9b22221435478
Vishal <vishal.smak214@gmail.com>
2025-07-25 12:00:24 +0100

    Setting up dataset downloads

--- scripts/download_digitv1_dataset.sh
+++ scripts/download_digitv1_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set path to the directory to donwload the dataset
-path_dataset="/media/chiguera/GUM/datasets/"
+path_dataset="/home/vishal/research/sparsh/datasets/"
 
 mkdir -p $path_dataset/digitv1
 cd $path_dataset/digitv1
